9c65be7de1a15728dce9f12c31a3d5f082040735
ivan telles <tellezivan7@gmail.com>
2024-02-04 20:59:33 -0800

    Add API key, gitignore and migration files
    The commit adds the API key as outlined in the .env file and also includes the .gitignore file. Multiple migration files have also been added, enabling changes such as the creation of the "portfolios" table and revisions to the "Comments" relationship to ensure it is one-to-one with the "AppUser".

---
+++ api/Migrations/20240131054556_comment-one-to-one.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class commentonetoone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a16521b7-e765-400d-a8e6-8c717fc5c199");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b05ff72f-1179-4bf8-a046-e908cd0484bf");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AppUserId",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "544e2097-13af-4c1d-a2da-f46b62a61a0f", null, "Admin", "ADMIN" },
+                    { "f10f3f2b-7463-4907-9f6c-f73676c66539", null, "User", "USER" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AppUserId",
+                table: "Comments",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_AppUserId",
+                table: "Comments",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_AppUserId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_AppUserId",
+                table: "Comments");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "544e2097-13af-4c1d-a2da-f46b62a61a0f");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f10f3f2b-7463-4907-9f6c-f73676c66539");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "Comments");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "a16521b7-e765-400d-a8e6-8c717fc5c199", null, "Admin", "ADMIN" },
+                    { "b05ff72f-1179-4bf8-a046-e908cd0484bf", null, "User", "USER" }
+                });
+        }
+    }
+}
